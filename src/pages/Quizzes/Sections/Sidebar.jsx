a24a0195a2298af26cb8a61915724ad525500568
Adarsh Rangare <akrangare@gmail.com>
2024-02-27 14:48:04 +0530

    fixed Mobile Layout

--- src/pages/Quizzes/Sections/Sidebar.jsx
+++ src/pages/Quizzes/Sections/Sidebar.jsx
@@ -4,12 +4,12 @@ const Sidebar = ({ classname, sectionList, selectedSection,handleChange }) => {
   console.log(sectionList);
   return (
     <aside
-      className={`min-h-screen ${classname}  border-r-2 flex flex-col p-4 gap-2   `}
+      className={` h-fit w-full overflow-x-scroll flex-row md:min-h-screen ${classname} max-md:border-b-2 max-md:py-2 px-4  md:border-r-2 flex md:flex-col md:p-4 mx-auto gap-2   `}
     >
       {sectionList?.map((sectionName, index) => (
         <div
           key={index}
-          className={`px-4 py-2 font-medium text-center rounded-full border-2 transition-all cursor-pointer ${
+          className={`px-4 max-md:text-nowrap py-2 max-md:text-sm font-medium text-center rounded-full border-2 transition-all cursor-pointer ${
             selectedSection === index ? "bg-sky-300" : ""
           }`}
           onClick={()=>{
